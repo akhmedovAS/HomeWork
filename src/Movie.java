e5b0873503bb17f90a555c0088876aa0814da018
akhmedovAS <ovecher86@ya.ru>
2022-05-29 17:54:00 +0300

    Программа, позволяющая получать данные о фильме, или менять их.

--- src/Movie.java
+++ src/Movie.java
@@ -1,34 +1,68 @@
+/**
+ * Класс, описывающий объект "Фильм"
+ */
 public class Movie {
     String director;
     String title;
     double duration;
 
+    /**
+     *
+     * @param director Имя режиссера
+     * @param title Название фильма
+     * @param duration Длительность фильма
+     */
     Movie(String director, String title, double duration) {
         this.director = director;
         this.title = title;
         this.duration = duration;
     }
 
+    /**
+     *
+     * @param director Установить имя режиссера
+     */
+
     public void setDirector(String director) {
         this.director = director;
     }
 
+    /**
+     *
+     * @param title установить название фильма
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     *
+     * @param duration установить длительность фильма
+     */
     public void setDuration(double duration) {
         this.duration = duration;
     }
 
+    /**
+     *
+     * @return получить имя режиссера
+     */
     public String getDirector() {
         return director;
     }
 
+    /**
+     *
+     * @return получить длительность фильма
+     */
     public double getDuration() {
         return duration;
     }
 
+    /**
+     *
+     * @return получить название фильма
+     */
     public String getTitle() {
         return title;
     }
---
+++ src/MovieTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MovieTest {
+
+    @Test
+    public void setDirector() {
+    }
+
+    @Test
+    public void setTitle() {
+    }
+
+    @Test
+    public void setDuration() {
+    }
+
+    @Test
+    public void getDirector() {
+    }
+
+    @Test
+    public void getDuration() {
+    }
+
+    @Test
+    public void getTitle() {
+    }
+}
