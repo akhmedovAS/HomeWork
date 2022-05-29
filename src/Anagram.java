89d19f3cb04ee58e5244778af91c4123d6c9282b
akhmedovAS <ovecher86@ya.ru>
2022-05-29 17:44:48 +0300

    Программа, составляющая анаграмму к слову, которое ввел пользователь.

---
+++ src/Anagram.java
@@ -0,0 +1,25 @@
+
+public class Anagram {
+
+    String userWord;
+
+    Anagram(String userWord){
+        this.userWord = userWord;
+    }
+
+
+
+
+    public String getAnagram(String userWord){
+        String[] array = userWord.split("");
+        String temp;
+
+        temp=array[0];
+        array[0]=array[array.length-1];
+        array[array.length-1]=temp;
+
+        userWord = String.join("",array);
+        return userWord;
+    }
+
+}
