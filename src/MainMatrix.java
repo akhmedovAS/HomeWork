60c3a155b4ba3b61f09e260efa62a5bd9672acbb
akhmedovAS <ovecher86@ya.ru>
2022-05-29 17:53:14 +0300

    Программа, позволяющая получать матрицу, складывать их, умножать или менять значения.

--- src/MainMatrix.java
+++ src/MainMatrix.java
@@ -3,5 +3,6 @@ public class MainMatrix {
         Matrix matrix = new Matrix();
 
         matrix.addition(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}});
+        System.out.println(matrix.getSideMLenght());
     }
 }
